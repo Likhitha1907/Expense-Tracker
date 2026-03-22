89c2b8d04151bd75bc343ec04ea7930ecdc0df5a
itsrajivv <itsrajivv@gmail.com>
2026-03-22 23:40:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // ===== GOOGLE AUTH =====
 passport.use(new GoogleStrategy({
-  clientID: "YOUR_CLIENT_ID",
-  clientSecret: "YOUR_CLIENT_SECRET",
-  callbackURL: "/auth/google/callback"
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
